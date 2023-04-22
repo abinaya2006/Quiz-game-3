219a24caaad3012ebe5345a26d727e17f3f127f5
abinaya2006 <72507845+abinaya2006@users.noreply.github.com>
2023-04-22 19:03:57 +0530

    Add files via upload

---
+++ quiz_client.py
@@ -0,0 +1,81 @@
+import socket
+from threading import Thread
+from tkinter import *
+
+
+
+# nickname=input("Enter your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address='127.0.0.1'
+port = 8000
+
+client.connect((ip_address,port))
+print("Connected with the server...")
+
+
+
+class GUI:
+
+
+        def goAhead(self,name):
+            self.login.destroy()
+            self.name = name
+            rec=Thread(target=self.receive)
+            rec.start()
+        
+        def receive(self):
+            while True:
+                try:
+                    message=client.recv(2048).decode('utf-8')
+                    if message == 'NICKNAME':
+                        client.send(self.name.encode('utf-8'))
+                    else:
+                        pass
+                except:
+                    print("An error occured!")
+                    client.close()
+                    break
+            
+
+        def __init__(self):
+            self.Window= Tk()
+            self.Window.withdraw()
+
+            self.login = Toplevel()
+            self.login.title("Login")
+            self.login.resizable(width=False,height=False)
+            self.login.configure(width=400,height=300)
+            
+            self.label1= Label(self.login,text="Please login to continue...",font="Helvetica 14 bold")
+            self.label1.place(relheight=0.15,relx=0.2,rely=0)
+
+            self.name = Label(self.login,text="Name: ",font="Helvetica 10",)  
+            self.name.place(   relheight = 0.2,relx = 0.1,rely = 0.2)
+
+            self.entryName = Entry(self.login,font="Helvetic 12")
+            self.entryName.place(relwidth = 0.4,relheight = 0.12,relx = 0.35,rely = 0.2)
+            self.entryName.focus()
+
+            self.go = Button(self.login,text = "CONTINUE",font = "Helvetica 14 bold",
+                command = lambda: self.goAhead(self.entryName.get()))
+            self.go.place(  relx = 0.4,rely = 0.55)
+            
+            self.Window.mainloop()
+
+
+
+
+g=GUI()
+
+'''def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('utf-8'))
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+write_thread = Thread(target=write)
+write_thread.start()'''
+
